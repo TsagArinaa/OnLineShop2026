29366d6ed40deb06299da3949cb66f2459ad1550
TsagArinaa <tsagaraevaarina@gmail.com>
2026-04-07 23:23:44 +0300

    создала модель User

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PathImage { get; set; }
+
+        public User(string name, string surname, string email, string pathImage= "/images/woman_avatar.png")
+        {
+            UserId = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Email = email;
+            PathImage = pathImage;
+        }
+    }
+}
