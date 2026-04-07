2a2e0dc6d1ce5eeb7c73646988475e3d0cb21a49
TsagArinaa <tsagaraevaarina@gmail.com>
2026-04-07 23:45:33 +0300

    Создала Controller UserController

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            User user = new User("Арина", "Цагараева", "tsagaraevaarina@gmail.com");
+            return View(user);
+        }
+    }
+}
